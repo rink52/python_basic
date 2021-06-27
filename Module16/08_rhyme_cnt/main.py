33904221a5b750c435485860c5257e6993e14c0c
Михаил <rink@yandex.ru>
2021-06-27 14:27:41 +0300

    module_16 update task 8

--- Module16/08_rhyme_cnt/main.py
+++ Module16/08_rhyme_cnt/main.py
@@ -1,51 +1,20 @@
-def laps(players, shift_element_list):
-    print('\nТекущий круг людей: ', sorted(players))
-    print('Начало счета с номера: ', players[0])
-    print('Выбывает человек под номером: ', shift_element_list[0])
-    shift_element_list.pop(0)
-    players.clear()
-    return players
-
-count_players = int(input('Кол-во человек: '))
+players = int(input('Кол-во человек: '))
 number_kick = int(input('Какое число в считалке? '))
 print(f'Значит, выбывает каждый {number_kick} человек')
 players = list(range(1, count_players+1))
 
-while len(players) != 1:
-    shift_element_list = []
+index_del = 0
 
-    if len(players) >= number_kick:
-        for i in range(len(players)):
-            if (number_kick + i ) <= len(players):
-                shift_element_list.append(players[number_kick - 1 + i])
-            else:
-                for i in range(number_kick-1):
-                    shift_element_list.append(players[i])
-                laps(players, shift_element_list)
-                players = shift_element_list.copy()
-                break
+while len(players) > 1:
+    print('\nТекущий круг людей:', players)
+    index_start = index_del % len(players)
+    print('Начало счёта с номера', players[index_start])
 
-    elif len(players) < number_kick and number_kick // len(players) == 1:
-        for i in range(len(players)):
-            if (number_kick - len(players) - 1 + i) < len(players):
-                shift_element_list.append(players[(number_kick - len(players) - 1 + i)])
-                if len(players) == len(shift_element_list):
-                    laps(players, shift_element_list)
-                    players = shift_element_list.copy()
-                    break
-            else:
-                for i in range(number_kick - len(players) - 1):
-                    shift_element_list.append(players[i])
-                laps(players, shift_element_list)
-                players = shift_element_list.copy()
-                break
-    else:
-        for i in range(len(players)):
-            shift_element_list.append(players[(number_kick % len(players) - 1 + i)])
-        laps(players, shift_element_list)
-        players = shift_element_list.copy()
+    index_del = (index_start + number_kick - 1) % len(players)
+    print('Выбывает человек под номером', players[index_del])
+    players.remove(players[index_del])
 
-print(f'\nОстался человек под номером {players[0]}')
+print('\nОстался человек под номером', players[0])
 
 
 
