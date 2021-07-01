8fb17d46b08ef5d2995e48cb23de96f22b91de5d
Михаил <rink@yandex.ru>
2021-07-01 23:50:04 +0300

    module_17 home work complete

--- Module17/05_reversal/main.py
+++ Module17/05_reversal/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+text = 'тестовая строка h h ьсоличулоп ьтунревереп h и все рады'
+# text = input('Введите текст: ')
+
+str_text = [cnt
+            for cnt in range(len(text))
+            if text[cnt] == 'h']
+
+text_new = text[:str_text[0]] + text[str_text[-1]:str_text[0]:-1] + text[str_text[-1]:]
+
+print(text_new)
--- Module17/10_caesar_cipher/main.py
+++ Module17/10_caesar_cipher/main.py
@@ -1,1 +1,16 @@
-# TODO здесь писать код
+abc = ['а','б','в','г','д','е','ё','ж',
+       'з','и','й','к','л','м','н','о',
+       'п','р','с','т','у','ф','х','ц',
+       'ч','ш','щ','ъ','ы','ь','э','ю','я']
+
+text = (input('Введите сообщение: ')).lower()
+shift = int(input('Введите сдвиг: '))
+encrypt_text = ''
+for symbol in text:
+    if symbol == ' ':
+        encrypt_text += symbol
+    else:
+        encrypt_text += abc[(abc.index(symbol)+shift) % len(abc)]
+
+print('Зашифрованное сообщение: ', encrypt_text)
+
